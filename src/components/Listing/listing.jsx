148a40e278e9722eae951cd6f3cc26bad0723bcb
unknown <f20201815@pilani.bits-pilani.ac.in>
2024-07-23 17:52:14 +0530

    Created movie cards, added rendering as a list, added styling for UI

--- src/components/Listing/listing.jsx
+++ src/components/Listing/listing.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMovies } from '../../redux/movieSlice.jsx';
+import MovieCard from './movie.jsx'
+import './listing.scss'
 
 const listing = () => {
+
+    const movies = useSelector(getAllMovies)
+    let render = movies.Response === "True" ? (
+        movies.Search.map((movie, index) => {
+            return <MovieCard key = {index} data = {movie}></MovieCard>
+        })
+    ) : (
+        <div className='movie-error'>
+            <h3>
+                {movies.Error}
+            </h3>
+        </div>
+    );
+
     return (
-        <div>
-            Listing
+        <div className='movie-outer'>
+            <div className='list'>
+                <div className='movie-container'>
+                    {render}
+                </div>
+            </div>
         </div>
     )
 };
--- src/components/Listing/movie.jsx
+++ src/components/Listing/movie.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import './movie.scss'
 
-const movie = () => {
+const MovieCard = (props) => {
+    const {data} = props;
+
     return (
-        <div>
+        <div className='card'>
+            <div className='card-border'>
+                <div className='card-top'>
+                    <h2>
+                        {data.Title}
+                        <h4>{data.Year}</h4>
+                    </h2>
+                    <img src={data.Poster} alt={data.Title} className='image'></img>
+                </div>
+                <div className='card-bottom'>
+                </div>
+            </div>
             
         </div>
     );
 };
 
-export default movie;
+export default MovieCard;
